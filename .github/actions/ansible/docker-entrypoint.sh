733c65f41592e0cf8e8e372e0da2a034ede8c146
Louis-Olivier Guérin <hi@louisolivier.com>
2020-01-19 00:23:45 -0500

    Remove unecessary lines

--- .github/actions/ansible/docker-entrypoint.sh
+++ .github/actions/ansible/docker-entrypoint.sh
@@ -2,9 +2,6 @@
 
 echo "$VAULT_PASS" > /.vault_pass.txt
 
-mkdir -p /etc/ansible
-echo "$SERVER_IP" > /etc/ansible/hosts
-
 mkdir ~/.ssh
 
 ansible-vault view /ssh_key --vault-password-file="/.vault_pass.txt" > ~/.ssh/id_rsa
