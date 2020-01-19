4a0d68ceb066ca9e4d48b8c6351ae415e99a6d88
Louis-Olivier Guérin <hi@louisolivier.com>
2020-01-18 23:57:51 -0500

    Remove tilde

--- .github/actions/ansible/docker-entrypoint.sh
+++ .github/actions/ansible/docker-entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-echo "$VAULT_PASS" > ~/.vault_pass.txt
+echo "$VAULT_PASS" > /.vault_pass.txt
 
 mkdir -p /etc/ansible
 echo "$SERVER_IP" > /etc/ansible/hosts
 
 mkdir ~/.ssh
 
-ansible-vault --vault-password-file="~/.vault_pass.txt" view ~/ssh_key > ~/.ssh/id_rsa
+ansible-vault --vault-password-file="/.vault_pass.txt" view ~/ssh_key > ~/.ssh/id_rsa
 
 chmod 0600 ~/.ssh/id_rsa
 
