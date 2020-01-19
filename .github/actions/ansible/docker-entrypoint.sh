377f35ca95bd16ced67e79043cfe753e80a8c265
Louis-Olivier Guérin <hi@louisolivier.com>
2020-01-19 01:22:53 -0500

    Change dir structure

--- .github/actions/ansible/docker-entrypoint.sh
+++ .github/actions/ansible/docker-entrypoint.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
-echo "$VAULT_PASS" > /.vault_pass.txt
+echo "$VAULT_PASS" > /ansible/.vault_pass
 
 mkdir ~/.ssh
 
-ansible-vault view /ssh_key --vault-password-file="/.vault_pass.txt" > ~/.ssh/id_rsa
+ansible-vault view /ssh_key --vault-password-file="/ansible/.vault_pass" > ~/.ssh/id_rsa
 
 chmod 0600 ~/.ssh/id_rsa
 
-ansible all \
+#ansible-playbook \
+#  -e "build_sha=$GITHUB_SHA" \
+#  --vault-password-file="/.vault_pass.txt" \
+#  -i web-servers \
+#  /ansible/deploy.yml
+ansible \
+  -i web-servers \
   -m ping \
   -u githubactions
