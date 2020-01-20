9aa0f4abdf43a0e4638f84d08113f5f9c8b1b9b2
Louis-Olivier Guérin <hi@louisolivier.com>
2020-01-19 20:49:45 -0500

    Test with playbook

--- .github/actions/ansible/docker-entrypoint.sh
+++ .github/actions/ansible/docker-entrypoint.sh
@@ -8,11 +8,7 @@ ansible-vault view /ssh_key --vault-password-file="/ansible/.vault_pass" > ~/.ss
 
 chmod 0600 ~/.ssh/id_rsa
 
-#ansible-playbook \
-#  -e "build_sha=$GITHUB_SHA" \
-#  --vault-password-file="/.vault_pass.txt" \
-#  -i web-servers \
-#  /ansible/deploy.yml
-ansible web-servers \
-  -m ping \
-  -u githubactions
+ansible-playbook \
+  -e "build_sha=$GITHUB_SHA" \
+  --vault-password-file="/.vault_pass.txt" \
+  /ansible/deploy.yml
